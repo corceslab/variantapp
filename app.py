5f66f5e248f1c98321a7e904be4cd954e514dcfb
EC2 Default User <ec2-user@ip-172-31-29-6.us-east-2.compute.internal>
2021-07-18 06:12:30 +0000

    moved app.run into guard block for gunicorn

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def home():
 #         return render_template('output.html')
 #     return render_template('index_values.html', form=form)
 
-app.run(host='0.0.0.0', port=50000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
